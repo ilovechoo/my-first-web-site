cef9bdaa35f6809a542aa05c6876181bfa42444d
ilovechoo <wldms5221@icloud.com>
2023-08-08 15:23:24 +0900

    8/8(2)

--- JavaScript/colors.js
+++ JavaScript/colors.js
@@ -1,30 +1,32 @@
 var Links = {
     setcolor: function(color) {
-    var alist = document.querySelectorAll('a');
-    var i =0;
-    while(i < alist.length){
-        alist[i].style.color = color;
-        i = i + 1; 
-     }
-    }
-    }
-  var Body = {
-    setColor:function (color){
-        document.querySelector('body').style.color = color;
-  },
-   setBackgroundColor:function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+    // var alist = document.querySelectorAll('a');
+    // var i =0;
+    // while(i < alist.length){
+    //     alist[i].style.color = color;
+    //     i = i + 1; 
+    //  }
+    $('a').css('color', color);
     }
 }
-    
-    function nightDayHandler(self){
+  var Body = {
+    setColor:function (color){
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+  },
+   setBackgroundColor:function (color){
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+    }
+}
+function nightDayHandler(self){
      var target = document.querySelector('body');
     if(self.value === 'night') {
     Body.setBackgroundColor('black');
     Body.setColor('white');
     self.value = 'day';
 
-    Links.setcolor('powderblue');
+    Links.setcolor('yellow');
     } else {
     Body.setBackgroundColor('white');
     Body.setColor('black');
